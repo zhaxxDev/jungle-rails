f62277636f92f4b2608092a0a2a3fd4649874986
zhaxxdev <alex.xian@Hotmail.com>
2021-01-10 16:34:00 -0800

    added user login

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,21 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.integer  "category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "names", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   add_foreign_key "line_items", "orders"
